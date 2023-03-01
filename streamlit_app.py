bbbadc2ce3e9f12ec9f124f86a9ddf11054d4b2f
akashChouksey <84224338+akashChouksey@users.noreply.github.com>
2023-03-01 14:12:06 +0530

    Update streamlit_app.py
    add header and text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('Hello World!!!')
+
+streamlit.header('Breakfast menu')
+streamlit.text('Omega 3 and Bulebarry oatmeal')
+streamlit.text('kale, Spinach & Rocket Smoothie')
+streamit.text('Hard-Boiled Free-Rnge Egg')
